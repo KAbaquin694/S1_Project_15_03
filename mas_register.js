9e117acc03089beff8c31872e203b83b20e73261
Khalel Abaquin <kabaquin694@west-mec.org>
2019-04-22 15:20:12 -0700

    Update 2
    added variables to the 'calcCart' function, added a couple of comments, & added framework for an if statement (having a bit of trouble)

--- mas_register.js
+++ mas_register.js
@@ -28,6 +28,7 @@
 
 */
 
+//This section of code adds an event listener that loads an anonymous function when the browser loads. The anonymous function calls the 'calcCart' function & runs 4 document methods.
 window.addEventListener("load", function () {
       calcCart();
       document.getElementById("regSubmit").onclick = sessionTest;
@@ -36,6 +37,7 @@ window.addEventListener("load", function () {
       document.getElementById("checkBox").onclick = calcCart;
 });
 
+//This block of code creates the 'sessionTest' function. Inside the function is an if statement that creates a custom validity message if the user has not put in the correct information.
 function sessionTest() {
       if (sessionBox[i] !== -1) {
             sessionBox.setCustomValidity("Select a Session Package");
@@ -44,6 +46,17 @@ function sessionTest() {
       }
 }
 
-function calccart() {
-      var confName = "" + +" " + +"";
+//
+function calcCart() {
+      var confName = "" + document.regForm.firstName.value + " " + document.regForm.lastName.value + "";
+      var confGroup = document.regForm.group.value;
+      var confMail = document.regForm.email.value;
+      var confPhone = document.regForm.phoneNumber.value;
+      var confBanquet = document.regForm.banquetGuests.value;
+      var confBanquetCost = confBanquet * 55;
+      if (sessionBox[i] !== -1) {
+            // var confSession = ;
+      } else {
+
+      }
 }
